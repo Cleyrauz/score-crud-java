8e37fa6dcf5c512d7eda0c4d8528b71eb0cd3eba
cheche200 <joseenrique86@gmail.com>
2018-02-25 20:59:19 +0000

    Added Score Object

--- src/main/java/com/orchestra/model/Score.java
+++ src/main/java/com/orchestra/model/Score.java
@@ -1,4 +1,19 @@
 package com.orchestra.model;
 
 public class Score {
+
+    public String tittle;
+
+    public int year;
+
+    public String author;
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "tittle='" + tittle + '\'' +
+                ", year=" + year +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
